28133cc8c9a042b1b89db91e06a356395d593db9
nadahmd <shaikhnavaid10@gmail.com>
2019-06-29 15:16:55 -0700

    first commit

---
+++ contracts/DappToken.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.040;
+
+dontract DappToken {
+//constructor
+//set the total number of tokens 
+//Read the total number of tokens
+uin256 public totalSupply;
+
+function DappToken () public {
+    totalSupply = 1000000;
+}    
+}
